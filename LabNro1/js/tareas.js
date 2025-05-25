5f949bd323e72e92fdbbef4d398056174ca629d4
BrunoAguinaga0 <brunoaguinaga0@gmail.com>
2025-05-25 04:38:05 -0300

    new commit

--- LabNro1/js/tareas.js
+++ LabNro1/js/tareas.js
@@ -5,40 +5,51 @@ document.addEventListener('DOMContentLoaded', function () {
     let limpiarTareas = document.getElementById("btnLimpiar")
     var contenedor = document.getElementById("espacioTareas")
     var contenedor2 = document.getElementById("espacioTareas2")
-
+    
     ingreso.addEventListener('click', function () {
         let descripcion = document.getElementById("descripcion").value
         if (descripcion != "") {
-        let parrafo = document.createElement("p")
-        parrafo.innerHTML = descripcion
-        let nuevoInput = document.createElement("input")
-        nuevoInput.setAttribute("type", "checkbox")
-        contenedor.appendChild(nuevoInput)
-        contenedor.appendChild(parrafo)
-        }else {
+            let div = document.createElement("div")
+            div.setAttribute("class","filaTarea")
+            let parrafo = document.createElement("p")
+            parrafo.innerHTML = descripcion
+            let nuevoInput = document.createElement("input")
+            nuevoInput.setAttribute("type", "checkbox")
+            div.appendChild(nuevoInput)
+            div.appendChild(parrafo)
+            contenedor.appendChild(div)
+        } else {
             alert("Ingrese una tarea.")
         }
     })
 
     finalizo.addEventListener('click', function () {
-        let checkboxes = contenedor.querySelectorAll("input[type='checkbox']")
-        for (let checkbox of checkboxes) {
-            if (checkbox.checked) {
-                let parrafo = checkbox.nextElementSibling
-                contenedor.removeChild(parrafo)
-                contenedor.removeChild(checkbox)
-                contenedor2.appendChild(parrafo)
+        var divs = document.querySelectorAll(".filaTarea")
+        for(let div of divs){
+            let checkboxes = div.querySelectorAll("input[type='checkbox']")
+            for (let checkbox of checkboxes) {
+                if (checkbox.checked) {
+                    let parrafo = checkbox.nextElementSibling
+                    div.removeChild(parrafo)
+                    div.removeChild(checkbox)
+                    contenedor2.appendChild(parrafo)
+                    contenedor.removeChild(div)
+                }
             }
         }
     })
     limpiarTareas.addEventListener('click', function () {
-        let checkboxes = contenedor.querySelectorAll("input[type='checkbox']")
-        for (let checkbox of checkboxes) {
-            let parrafo = checkbox.nextElementSibling
-            contenedor.removeChild(parrafo)
-            contenedor.removeChild(checkbox)
+        var divs = document.querySelectorAll(".filaTarea")
+        for(let div of divs){
+            let checkboxes = div.querySelectorAll("input[type='checkbox']")
+            for (let checkbox of checkboxes) {
+                let parrafo = checkbox.nextElementSibling
+                div.removeChild(parrafo)
+                div.removeChild(checkbox)
+                contenedor.removeChild(div)
+            }
         }
         contenedor2.innerHTML = ""
-        
+
     })
 })
